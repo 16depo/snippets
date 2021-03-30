a6fa7fb03aa454038e8754a15cc46d9c76ef172c
Kirill Matvienko (depo) <depo@yandex-team.ru>
2021-03-30 17:39:18 +0300

    Calculate table

--- freq.py
+++ freq.py
@@ -15,7 +15,7 @@ def conversion(freq, current_prescaler, fill_percent, print_result):
         return 0
     if print_result == True:
         print(
-        f'CurrentFreq {current_freq}, Autoreload {autoreload}, SetFreq {freq}, CalcFreq: {conversionFreq}, Compare: {compare}, Prescaler: {current_prescaler}')
+        f'SetFreq {freq}, CalcFreq: {conversionFreq:10f}, CurrentFreq {int(current_freq):11}, Autoreload {int(autoreload):6}, Compare: {compare:7}, Prescaler: {current_prescaler:6}')
     return conversionFreq
 
 
@@ -35,22 +35,24 @@ def prescaler_rough_seacrh(freq):
 
 
 if __name__ == '__main__':
-    frequency = int(input('Enter freq:\n'))
-    scale = prescaler_rough_seacrh(frequency)
-    result_freq = conversion(frequency, scale, 50,True)
-    accuracy = abs(result_freq - frequency)
-    if accuracy < 0.1:
-        print(f'Accurate enough, accuracy is {accuracy}')
-        sys.exit()
-    else:
-        print(f'Not accurate enough, accuracy is {accuracy}')
-        max_accuracy = 1000
-        max_accuracy_scale = scale
-        for i in range(1, 65535):
-            accuracy = abs(frequency - conversion(frequency, i, 50, False))
-            if accuracy < max_accuracy:
-                max_accuracy = accuracy
-                max_accuracy_scale = i
-            if max_accuracy < 0.1:
-                break
-        conversion(frequency, max_accuracy_scale, 50, True)
+    for i in range(1,10001):
+        frequency = i
+        target_accuracy = i/10000
+        scale = prescaler_rough_seacrh(frequency)
+        result_freq = conversion(frequency, scale, 50,False)
+        accuracy = abs(result_freq - frequency)
+        if accuracy < target_accuracy:
+            conversion(frequency, scale, 50, True)
+            continue
+        else:
+            #print(f'Not accurate enough, accuracy is {accuracy}')
+            max_accuracy = 1000
+            max_accuracy_scale = scale
+            for i in range(1, 65535):
+                accuracy = abs(frequency - conversion(frequency, i, 50, False))
+                if accuracy < max_accuracy:
+                    max_accuracy = accuracy
+                    max_accuracy_scale = i
+                if max_accuracy < target_accuracy:
+                    break
+            conversion(frequency, max_accuracy_scale, 50, True)
