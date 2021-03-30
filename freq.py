12456a547ddb3a6c65d07c00b74b55e110a094ba
Kirill Matvienko (depo) <depo@yandex-team.ru>
2021-03-30 17:54:27 +0300

    Formatting

--- freq.py
+++ freq.py
@@ -15,7 +15,7 @@ def conversion(freq, current_prescaler, fill_percent, print_result):
         return 0
     if print_result == True:
         print(
-        f'SetFreq {freq}, CalcFreq: {conversionFreq:10f}, CurrentFreq {int(current_freq):11}, Autoreload {int(autoreload):6}, Compare: {compare:7}, Prescaler: {current_prescaler:6}')
+            f'SetFreq {freq}, CalcFreq: {conversionFreq:10f}, CurrentFreq {int(current_freq):11}, Autoreload {int(autoreload):6}, Compare: {compare:7}, Prescaler: {current_prescaler:6}')
     return conversionFreq
 
 
@@ -37,20 +37,20 @@ def prescaler_rough_seacrh(freq):
 if __name__ == '__main__':
     input_accuracy = float(input('Введите желаемую точность(например 0.0001, по умолчанию динамическая)\n') or '-1')
     target_compare = int(input('Введите процент заполнения (compare, от 1% до 99%, по умолчанию 50%)\n') or '50')
-    for i in range(1,10001):
+    for i in range(1, 10001):
         frequency = i
         if input_accuracy == -1:
-            target_accuracy = frequency/10000
+            target_accuracy = frequency / 10000
         else:
-            target_accuracy=input_accuracy
+            target_accuracy = input_accuracy
         scale = prescaler_rough_seacrh(frequency)
-        result_freq = conversion(frequency, scale, target_compare,False)
+        result_freq = conversion(frequency, scale, target_compare, False)
         accuracy = abs(result_freq - frequency)
         if accuracy < target_accuracy:
             conversion(frequency, scale, target_compare, True)
             continue
         else:
-            #print(f'Not accurate enough, accuracy is {accuracy}')
+            # print(f'Not accurate enough, accuracy is {accuracy}')
             max_accuracy = 1000
             max_accuracy_scale = scale
             for i in range(1, 65535):
